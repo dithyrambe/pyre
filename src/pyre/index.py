58b7ba9a8072d8b5b8fdcd63052335fc048709e8
dithyrambe <dithyrambe@outlook.fr>
2024-06-02 00:19:10 +0200

    :bug: Interpolate values during closed market / missing values

--- src/pyre/index.py
+++ src/pyre/index.py
@@ -14,7 +14,12 @@ class Index:
     def get_historical_data(self, start_date: Date, end_date: Optional[Date] = None) -> pd.DataFrame:
         start = start_date.to_date_string()
         end = end_date.to_date_string() if end_date is not None else end_date
-        self.data = self.ticker.history(start=start, end=end)
+        data = self.ticker.history(start=start, end=end)
+
+        min_available = data.index.min()
+        max_available = data.index.max()
+        self.data = data.reindex(pd.date_range(min_available, max_available)).interpolate(method="linear")
+
 
     def get_variations(self) -> pd.Series:
         return self.data[CLOSING_COL].pct_change(fill_method=None)
