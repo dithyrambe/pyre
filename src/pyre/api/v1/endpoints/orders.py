fd3bbd2d3cbcd0a7c16c601f2cc46a170b070ae1
lvpasquier <louis-victor.pasquier@adevinta.com>
2024-06-11 20:27:02 +0200

    :dart: Allow filtering when retrieving orders

--- src/pyre/api/v1/endpoints/orders.py
+++ src/pyre/api/v1/endpoints/orders.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Optional
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
@@ -11,30 +11,35 @@ router = APIRouter(prefix="/orders")
 
 
 @router.get("/", response_model=List[Order])
-def get_orders(db: Session = Depends(get_db)):
-    orders = crud.get_orders(db=db)
+def get_orders(
+    ticker: Optional[str] = None,
+    start_datetime: Optional[str] = None,
+    end_datetime: Optional[str] = None,
+    db: Session = Depends(get_db)
+):
+    orders = crud.get_orders(db=db, ticker=ticker, start_datetime=start_datetime, end_datetime=end_datetime)
     return orders
     
 
 @router.get("/{id}", response_model=Order)
 def get_order_by_id(id: int, db: Session = Depends(get_db)):
-    order = crud.get_order_by_id(id=id, db=db)
+    order = crud.get_order_by_id(db=db, id=id)
     return order
 
 
 @router.post("/", response_model=Order)
 def upsert_order(order: Order, db: Session = Depends(get_db)):
-    upserted = crud.upsert_order(order=order.model_dump(), db=db)
+    upserted = crud.upsert_order(db=db, order=order.model_dump())
     return upserted
 
 
 @router.post("/bulk")
 def upsert_orders(orders: List[Order], db: Session = Depends(get_db)):
-    upserted = crud.bulk_upsert_order(orders=[o.model_dump() for o in orders], db=db)
+    upserted = crud.bulk_upsert_order(db=db, orders=[o.model_dump() for o in orders])
     return upserted
 
 
 @router.delete("/{id}")
 def delete_order_by_id(id: int, db: Session = Depends(get_db)):
-    crud.delete_order_by_id(id=id, db=db)
+    crud.delete_order_by_id(db=db, id=id)
 
--- tests/test_api.py
+++ tests/test_api.py
@@ -54,6 +54,32 @@ def test_get_orders(client: TestClient):
     assert orders[0]["id"] == 1
 
 
+def test_get_orders_filter_ticker(client: TestClient):
+    with Session(create_engine()) as db:
+        order = Order(id=2, datetime=pendulum.now(), ticker="DEF", price=10.0, quantity=2, fees=0.1)
+        db.add(order)
+        db.commit()
+    response = client.get("/v1/orders?ticker=DEF")
+    orders = response.json()
+    assert len(orders) == 1
+    assert orders[0]["id"] == 2
+
+
+def test_get_orders_filter_datetime(client: TestClient):
+    with Session(create_engine()) as db:
+        order2 = Order(id=2, datetime=pendulum.parse("2020-06-01"), ticker="DEF", price=10.0, quantity=2, fees=0.1)
+        order3 = Order(id=3, datetime=pendulum.parse("2020-06-02"), ticker="DEF", price=10.0, quantity=2, fees=0.1)
+        order4 = Order(id=4, datetime=pendulum.parse("2020-06-05"), ticker="DEF", price=10.0, quantity=2, fees=0.1)
+        db.add(order2)
+        db.add(order3)
+        db.add(order4)
+        db.commit()
+    response = client.get("/v1/orders?start_datetime=2020-06-01&end_datetime=2020-06-05")
+    orders = response.json()
+    assert len(orders) == 2
+    assert [order["id"] for order in orders] == [2, 3]
+
+
 def test_get_order_by_id(client: TestClient):
     response = client.get("/v1/orders/1")
     order = response.json()
