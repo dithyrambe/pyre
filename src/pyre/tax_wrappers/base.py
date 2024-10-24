6674c92b9b1344821718234f81e0bdd445cf1930
lvpasquier <louis-victor.pasquier@adevinta.com>
2024-10-24 02:56:53 +0200

    :receipt: Add tax wrappers

---
+++ src/pyre/tax_wrappers/base.py
@@ -0,0 +1,55 @@
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+
+from pendulum import DateTime
+import numpy as np
+import pendulum
+
+
+@dataclass
+class Contribution:
+    amount: float
+    datetime: DateTime
+
+@dataclass
+class Withdrawal:
+    pct: float
+    datetime: DateTime
+
+
+class TaxWrapper(ABC):
+    CONTRIBUTION_LIMIT = np.inf
+
+    def __init__(self, fees: float = 0.0, annualized_return: float = 0.0, opening_date: DateTime | None = None) -> None:
+        self.opening_date = opening_date or pendulum.now()
+        self.fees = fees
+        self.annualized_return = annualized_return - self.fees
+        self.monthly_return = (1 + self.annualized_return) ** (1 / 12) - 1
+        self.contributions: list[Contribution] = []
+
+    def total_contribution(self, datetime: DateTime):
+        return sum(_.amount for _ in filter(lambda contrib: contrib.datetime <= datetime, self.contributions))
+
+    def portfolio_value(self, datetime: DateTime):
+        months_since_contributions = [(datetime - contrib.datetime).in_months() for contrib in self.contributions]
+        compounded_contributions = [
+            contrib.amount * (1 + self.monthly_return) ** months
+            for contrib, months in zip(self.contributions, months_since_contributions)
+            if months >= 0
+        ]
+        return sum(compounded_contributions)
+
+    def gain(self, datetime: DateTime) -> float:
+        return self.portfolio_value(datetime) - self.total_contribution(datetime)
+
+    def contribute(self, contribution: Contribution) -> None:
+        if self.contributions and self.contributions[-1].datetime > contribution.datetime:
+            raise ValueError("Contribution is prior to last contribution")
+        if self.total_contribution(contribution.datetime) + contribution.amount > self.CONTRIBUTION_LIMIT:
+            raise ValueError(f"Contribution limit is {self.CONTRIBUTION_LIMIT}")
+        self.contributions.append(contribution)
+
+    @abstractmethod
+    def withdraw(self, withdrawal: Withdrawal) -> tuple[float, float]:
+        gross_withdraw = withdrawal.pct * self.portfolio_value(withdrawal.datetime)
+        return gross_withdraw
---
+++ tests/test_tax_wrappers/test_av.py
@@ -0,0 +1,52 @@
+from unittest.mock import patch
+
+import pendulum
+import pytest
+
+from pyre.tax_wrappers.base import Withdrawal
+from pyre.tax_wrappers.constants import ISR, PFU, PS
+from pyre.tax_wrappers.av import AV
+
+
+def test_av_before_min_holding():
+    opening = pendulum.parse("2025-01-01")
+    av = AV(opening_date=opening)
+
+    with (
+        patch.object(av, "total_contribution") as total_contribution,
+        patch.object(av, "portfolio_value") as portfolio_value
+    ):
+        total_contribution.return_value = 10_000
+        portfolio_value.return_value = 15_000
+
+        withdrawal = Withdrawal(pct=1.0, datetime=(opening + av.TAX_MINIMAL_HOLDING_PERIOD).subtract(days=1))
+        net, tax = av.withdraw(withdrawal=withdrawal)
+        expected_tax = 5_000 * PFU
+        expected_net = 15_000 - expected_tax
+        assert tax == expected_tax
+        assert net == expected_net
+
+
+@pytest.mark.parametrize(
+    argnames=("contribution", "gains", "expected_tax"),
+    argvalues=[
+        (10_000, 1_000, 1_000 * PS + max(0, 1_000 - AV.TAX_RELIEF) * AV.PF),
+        (50_000, 10_000, 10_000 * PS + max(0, 10_000 - AV.TAX_RELIEF) * AV.PF),
+        (200_000, 40_000, 40_000 * PS + max(0, 40_000 - AV.TAX_RELIEF) * (AV.PF * AV.SOFT_CONTRIBUTION_LIMIT + (200_000 - AV.SOFT_CONTRIBUTION_LIMIT) * ISR) / 200_000),
+    ]
+)
+def test_pea_after_min_holding(contribution: float, gains: float, expected_tax: float):
+    opening = pendulum.parse("2025-01-01")
+    av = AV(opening_date=opening)
+
+    with (
+        patch.object(av, "total_contribution") as total_contribution,
+        patch.object(av, "portfolio_value") as portfolio_value
+    ):
+        total_contribution.return_value = contribution
+        portfolio_value.return_value = contribution + gains
+
+        withdrawal = Withdrawal(pct=1.0, datetime=(opening + av.TAX_MINIMAL_HOLDING_PERIOD).add(days=1))
+        _, tax = av.withdraw(withdrawal=withdrawal)
+        assert tax == expected_tax
+
