732f503e62d913e52472e7ffcc1be5c44b3b89bf
lvpasquier <louis-victor.pasquier@adevinta.com>
2024-11-23 01:48:04 +0100

    :art: Rename tax_wrappers tests

---
+++ tests/tax_wrappers/test_cto.py
@@ -0,0 +1,28 @@
+from unittest.mock import patch
+
+import pendulum
+
+from pyre.tax_wrappers.base import Withdrawal
+from pyre.tax_wrappers.constants import PFU
+from pyre.tax_wrappers.cto import CTO
+
+
+def test_cto():
+    opening = pendulum.parse("2025-01-01")
+    pea = CTO(opening_date=opening)
+
+    with (
+        patch.object(pea, "total_contribution") as total_contribution,
+        patch.object(pea, "portfolio_value") as portfolio_value
+    ):
+        total_contribution.return_value = 10_000
+        portfolio_value.return_value = 15_000
+
+        withdrawal = Withdrawal(pct=1.0, datetime=(opening + pea.TAX_MINIMAL_HOLDING_PERIOD).subtract(years=1))
+        net, tax = pea.withdraw(withdrawal=withdrawal)
+        expected_tax = 5_000 * PFU
+        expected_net = 15_000 - expected_tax
+        assert tax == expected_tax
+        assert net == expected_net
+
+
