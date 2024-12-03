f457b08ef4f4f1f49d04f49e4a937b339784b7fd
dithyrambe <dithyrambe@outlook.fr>
2024-12-03 22:47:32 +0100

    :pencil: Update cto varname

--- tests/tax_wrappers/test_cto.py
+++ tests/tax_wrappers/test_cto.py
@@ -9,19 +9,17 @@ from pyre.tax_wrappers.cto import CTO
 
 def test_cto():
     opening = pendulum.parse("2025-01-01")
-    pea = CTO(opening_date=opening)
+    cto = CTO(opening_date=opening)
 
     with (
-        patch.object(pea, "total_contribution") as total_contribution,
-        patch.object(pea, "portfolio_value") as portfolio_value,
+        patch.object(cto, "total_contribution") as total_contribution,
+        patch.object(cto, "portfolio_value") as portfolio_value
     ):
         total_contribution.return_value = 10_000
         portfolio_value.return_value = 15_000
 
-        withdrawal = Withdrawal(
-            pct=1.0, datetime=(opening + pea.TAX_MINIMAL_HOLDING_PERIOD).subtract(years=1)
-        )
-        net, tax = pea.withdraw(withdrawal=withdrawal)
+        withdrawal = Withdrawal(pct=1.0, datetime=(opening + cto.TAX_MINIMAL_HOLDING_PERIOD).subtract(years=1))
+        net, tax = cto.withdraw(withdrawal=withdrawal)
         expected_tax = 5_000 * PFU
         expected_net = 15_000 - expected_tax
         assert tax == expected_tax
