d699cbb7aef49420f51b75871491a5d420e38e57
dithyrambe <dithyrambe@outlook.fr>
2024-12-08 02:09:36 +0100

    :video_game: Update sandbox

--- sandbox.py
+++ sandbox.py
@@ -5,55 +5,65 @@ from pyre.tax_wrappers.av import AV
 from pyre.tax_wrappers.base import Contribution, Withdrawal
 from pyre.tax_wrappers.cto import CTO
 from pyre.tax_wrappers.pea import PEA
+from rich.progress import track
 
 plt.style.use("ggplot")
 
 
-INVESTMENT_DURATION = pendulum.Duration(years=20)
-AMOUNT = 3000.0
+AMOUNT = 10_000.0
 PCT = 0.035
-START = pendulum.parse("2024-11-01")
+INVESTMENT_DURATION = pendulum.Duration(years=15)
+START = pendulum.parse("2025-01-01")
 END = START + INVESTMENT_DURATION
-ANNUALIZED_RETURN = 0.075
-INTERVAL = END - START
-
-pea = PEA(fees=0.00, annualized_return=ANNUALIZED_RETURN, opening_date=START)  # type: ignore
-cto = CTO(fees=0.00, annualized_return=ANNUALIZED_RETURN, opening_date=START)  # type: ignore
-av = AV(fees=0.010, annualized_return=ANNUALIZED_RETURN, opening_date=START)  # type: ignore
+DEATH = pendulum.parse("2091-01-01")
+ANNUALIZED_RETURN = 0.07
+INFLATION = 0.03
+ADJUSTED_ANNUALIZED_RETURN = ANNUALIZED_RETURN - INFLATION
+INTERVAL = DEATH - START
 
 data = {
     _: []
     for _ in [
         "date",
-        "pea",
-        "cto",
-        "av",
+        "pea_net",
+        "cto_net",
+        "pea_tax",
+        "cto_tax",
     ]
 }
 
-for dt in INTERVAL.range("months"):
+pea = PEA(fees=0.003, annualized_return=ADJUSTED_ANNUALIZED_RETURN, opening_date=START)  # type: ignore
+cto = CTO(fees=0.003, annualized_return=ADJUSTED_ANNUALIZED_RETURN, opening_date=START)  # type: ignore
+
+for dt in track([*INTERVAL.range("years")]):
     contribution = Contribution(amount=AMOUNT, datetime=dt)
     withdrawal = Withdrawal(pct=PCT, datetime=dt)
 
-    if pea.total_contribution(dt) + contribution.amount < pea.CONTRIBUTION_LIMIT:
-        pea.contribute(contribution=contribution)
-    else:
-        cto.contribute(contribution=contribution)
-        av.contribute(contribution=contribution)
+    if dt <= END:
+        if pea.total_contribution(dt) + contribution.amount < pea.CONTRIBUTION_LIMIT:
+            pea.contribute(contribution=contribution)
+        else:
+            cto.contribute(contribution=contribution)
 
-    pea_net, pea_tax = pea.withdraw(withdrawal=withdrawal)
-    cto_net, cto_tax = cto.withdraw(withdrawal=withdrawal)
-    av_net, av_tax = av.withdraw(withdrawal=withdrawal)
+    pea_net, pea_tax = pea.withdraw(withdrawal=withdrawal, dry_run=dt <= END)
+    cto_net, cto_tax = cto.withdraw(withdrawal=withdrawal, dry_run=dt <= END)
+
     data["date"].append(dt)
-    data["pea"].append(pea_net)
-    data["cto"].append(cto_net)
-    data["av"].append(av_net)
+    data["pea_net"].append(pea_net)
+    data["cto_net"].append(cto_net)
+    data["pea_tax"].append(pea_tax)
+    data["cto_tax"].append(cto_tax)
 
 df = pd.DataFrame(data).set_index("date")
-df["Rente pea+cto"] = df["pea"] + df["cto"]
-df["Rente pea+av"] = df["pea"] + df["av"]
-(df[["Rente pea+cto", "Rente pea+av"]]).plot.line(
-    figsize=(15, 10), legend=True, title=f"DCA €{AMOUNT} / month for {INTERVAL.in_years()} years"
+df["Rente pea+cto"] = df["pea_net"] + df["cto_net"]
+df["Taxe pea+cto"] = df["pea_tax"] + df["cto_tax"]
+
+ax = (df[["Rente pea+cto", "Taxe pea+cto"]]).plot.area(
+    figsize=(15, 10),
+    legend=True,
+    title=f"DCA €{AMOUNT} / month for {INVESTMENT_DURATION.years} years",
+    stacked=True,
+    alpha=0.7,
 )
 plt.show()
 plt.savefig("/tmp/fig.png", format="png")
